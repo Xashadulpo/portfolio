eeb2ddad0a7b84f76e39dfb6c3b9316e10da5414
xashadulpo <xashadulasif@gmail.com>
2024-06-25 13:23:26 +0600

    full about section done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/config/**/*.{js,ts,jsx,tsx,mdx}",
+
+
   ],
   theme: {
     extend: {
